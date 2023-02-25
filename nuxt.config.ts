70abbd5d68d0864b2ef15193c728b672896759c1
Roga <ryanroga@gmail.com>
2023-02-25 15:16:49 -0800

    Nuxt Project Reset
    Having numerous issues with different headless CMS setups via Nuxt. Tried Butter, Strapi, and of course Storyblok. Each having their own set of challenges, these may not be right for the intended purpose, or for ease of setup and integration.

--- nuxt.config.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss',
-    ["@storyblok/nuxt", { accessToken: process.env.STORYBLOK_ACCESS_TOKEN }]
-  ]
-});
--- pages/index.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-const story = await useAsyncStoryblok("home", { version: "draft" });
-</script>
-<template>
-  <div>
-    <h1>Testing Storyblok</h1>
-    <StoryblokComponent v-if="story" :blok="story.content" />
-  </div>
-</template>
