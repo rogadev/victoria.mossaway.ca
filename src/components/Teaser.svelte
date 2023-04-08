1f8ecb4b35528e59d0b63ac6d54342995c475b01
Roga <ryanroga@gmail.com>
2023-04-07 20:52:13 -0700

    Nuke

--- src/components/Teaser.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { storyblokEditable } from '@storyblok/svelte';
-
-	export let blok: { headline: string } = { headline: '' };
-</script>
-
-<div use:storyblokEditable={blok} class="py-32 text-6xl text-[#50b0ae] font-bold text-center">
-	{blok.headline}
-</div>
