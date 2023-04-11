f4b58c7fb2e0fe515da73f06f62d11d60c9f792d
Roga <ryanroga@gmail.com>
2023-04-11 11:48:37 -0700

    Updating Styles & Layout
    - Updated the home page
    
    - Created a consts folder and moved const values into relevant files within it
    
    - Added some SEO blueprints
    
    - Added TailwindCSS and created a rough theme.

---
+++ src/consts/image.ts
@@ -0,0 +1,46 @@
+import { home, mossRemoval, roofMossTreatment, windowCleaning, gutterCleaning, reviews } from './page';
+
+export const heroImage = {
+  src: "/background-image.jpg",
+  alt: "Beautiful moss-free roof after MossAway treatment",
+};
+
+export const mossRemovalImage = {
+  src: "/moss-removal.jpg",
+  alt: "Effective moss removal services by MossAway",
+  href: mossRemoval.link,
+};
+
+export const roofMossTreatmentImage = {
+  src: "/roof-moss-treatment.jpg",
+  alt: "MossAway team performing roof moss treatment",
+  href: roofMossTreatment.link,
+};
+
+export const windowCleaningImage = {
+  src: "/window-cleaning.jpg",
+  alt: "Professional window cleaning services by MossAway",
+  href: windowCleaning.link,
+};
+
+export const gutterCleaningImage = {
+  src: "/gutter-cleaning.jpg",
+  alt: "Expert gutter cleaning services in Victoria, BC",
+  href: gutterCleaning.link,
+};
+
+export const awardsImage = {
+  src: "/awards-image.png",
+  alt: "Awards or certifications",
+};
+
+export const reviewsImage = {
+  src: "/reviews-image.png",
+  alt: "Reviews or ratings",
+  href: reviews.link,
+};
+
+export const guaranteeImage = {
+  src: "/guarantee-image.png",
+  alt: "Satisfaction guarantee",
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  mode: 'jit',
+  purge: ['./src/**/*.astro'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: '#345c4c',
+      secondary: '#ffd700',
+      background: '#fefefd',
+      text: '#010102',
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
+    fontWeight: {
+      hairline: 100,
+      thin: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+      black: 900,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
