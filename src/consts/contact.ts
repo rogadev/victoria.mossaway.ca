38687604f1c7c7b8e253ee8738cb1864e2e3beb6
Roga <61405141+rogadev@users.noreply.github.com>
2023-04-11 12:10:44 -0700

    Astro (#54)
    * Initialized Astro Blog
    
    * Stubbing Out & Testing Component Usage
    
    * Updating Styles & Layout
    
    - Updated the home page
    
    - Created a consts folder and moved const values into relevant files within it
    
    - Added some SEO blueprints
    
    - Added TailwindCSS and created a rough theme.
    
    * Updated for Image Constants
    
    * Fixed Import Issues
    
    * Update roof-moss-removal-victoria.astro

---
+++ src/consts/contact.ts
@@ -0,0 +1,17 @@
+export const companyName = "MossAway Roof Moss Removal";
+
+export const companyLegalName = "MossAway Services Inc.";
+
+export const address = "1234 MossAway Way, Victoria, BC, V8V 8V8";
+
+export const phone = {
+  primary: '250-555-5555',
+  billing: '250-555-5555',
+  payments: '250-555-5555',
+};
+
+export const email = {
+  primary: 'info@mossaway.ca',
+  billing: 'billing@mossaway.ca',
+  payments: 'payments@mossaway.ca',
+};
---
+++ src/consts/page.ts
@@ -0,0 +1,55 @@
+export const home = {
+  title: 'MossAway | Roof Moss Treatment, Window & Gutter Cleaning in Victoria, BC',
+  link: '/',
+  description: "MossAway offers award-winning roof moss treatment, window cleaning, and gutter cleaning services in Victoria, BC. Contact us for a free quote today!",
+  keywords: 'mossaway, roof moss treatment, window cleaning, gutter cleaning, moss removal, victoria bc'
+};
+
+export const mossRemoval = {
+  title: 'Moss Removal Services | MossAway Victoria, BC',
+  link: '/moss-removal',
+  description: 'Get professional moss removal services from MossAway in Victoria, BC. Safe, effective, and eco-friendly methods to remove moss from your roof.',
+  keywords: 'moss removal, moss removal services, roof moss removal, moss removal victoria bc',
+};
+
+export const roofMossTreatment = {
+  title: 'Roof Moss Treatment | MossAway Victoria, BC',
+  link: '/roof-moss-treatment',
+  description: "Prevent roof moss from growing on your roof with MossAway's roof moss treatment. Safe, effective, and affordable. Contact us for a free quote today!",
+  keywords: 'roof moss treatment, moss treatment for roof, roof moss treatment victoria bc',
+};
+
+export const windowCleaning = {
+  title: 'Window Cleaning Services | MossAway Victoria, BC',
+  link: '/window-cleaning',
+  description: 'Get professional window cleaning services from MossAway in Victoria, BC. Interior and exterior window cleaning available. Contact us for a free quote today!',
+  keywords: 'window cleaning, window cleaning services, professional window cleaning, window cleaning victoria bc',
+};
+
+export const gutterCleaning = {
+  title: 'Gutter Cleaning Services | MossAway Victoria, BC',
+  link: '/gutter-cleaning',
+  description: 'Protect your home from water damage with professional gutter cleaning services from MossAway in Victoria, BC. Contact us for a free quote today!',
+  keywords: 'gutter cleaning, gutter cleaning services, expert gutter cleaning, gutter cleaning victoria bc',
+};
+
+export const faq = {
+  title: 'Frequently Asked Questions | MossAway Victoria, BC',
+  link: '/faq',
+  description: 'Get answers to common questions about our services and read our latest blog posts on the MossAway FAQ page. Contact us for more information.',
+  keywords: 'mossaway faq, mossaway blog, mossaway news, mossaway updates, mossaway victoria bc',
+};
+
+export const contact = {
+  title: 'Contact MossAway | MossAway Victoria, BC',
+  link: '/contact',
+  description: 'Get in touch with MossAway in Victoria, BC for a free quote on our moss removal, roof moss treatment, window cleaning, and gutter cleaning services.',
+  keywords: 'contact mossaway, mossaway contact, mossaway victoria bc',
+};
+
+export const reviews = {
+  title: 'MossAway Reviews | Victoria, BC',
+  link: '/reviews',
+  description: 'Read MossAway reviews and ratings from satisfied customers in Victoria, BC. Contact us for a free quote on our moss removal, roof moss treatment, window cleaning, and gutter cleaning services.',
+  keywords: 'mossaway reviews, mossaway ratings, mossaway victoria bc',
+};
