218d210850371b7972826486fc7a77862261374c
Roga <61405141+rogadev@users.noreply.github.com>
2023-04-20 15:31:25 -0700

    Major Updates (#58)
    * Update README.md
    
    * Adding Favicon & Images
    
    * More Photos & Prep
    
    * WIP
    
    * April 16 Updates
    
    * Merge branch 'main' of https://github.com/rogadev/ITAS268_FinalProject into dev
    
    * Update BlogPost.astro
    
    * Layout and UI Improvements
    
    Fixed some minor issues with layouts and updated some of the UI, including mobile menu/hamburger menu.
    
    * Homepage Update
    
    - Created an HomeServicesArticle component
    - Added styling to create uniformity to height and button position across all articles.
    - Added styles to call to action button
    - Updated ButtonCTA component for added styling and interaction transitions
    
    * WIP Unified Header Layout & BaseLayout
    
    - Added some unified style to the BaseLayout
    - Trying to add image background to "hero section" of the base layout header
    - WIP
    
    * Major Updates
    
    - Changed the const structure
    - Changed the URL paths names for SEO reasons
    - Changed the style and theme of some areas of the site
    - Added the Zoho forms web form and styled it accordingly

--- src/components/SiteNavbar.svelte
+++ src/components/SiteNavbar.svelte
@@ -16,12 +16,13 @@
   }
 </style>
 
-<div class="bg-background shadow-md">
+<div class="bg-background shadow-md mb-6">
   <div class="container mx-auto px-4 py-3 flex justify-between items-center">
-    <!-- TODO Get brand logo properly rendered and use SEO best practices -->
-    <img src="" alt="" />
-
-    <nav class="hidden lg:flex space-x-6">
+    <a href="/">
+      <img class="h-10 cursor-pointer my-1 mr-8" src="/img/moss/clump.png" alt='' />
+    </a>
+      
+    <nav class="hidden lg:flex lg:items-center space-x-6">
       {#each pages as page}
         <a
           href={page.link}
@@ -39,7 +40,7 @@
       <Hamburger on:toggle={toggleMobileMenu} ariaLabel="Toggle mobile menu" />
 
       {#if showMobileMenu}
-        <nav class="absolute right-0 mt-3 bg-background shadow-md rounded p-2 z-10">
+        <nav class="absolute right-0 mt-3 bg-background shadow-md rounded p-2 z-50">
           {#each pages as page}
             <a
               href={page.link}
--- src/consts/pages.ts
+++ src/consts/pages.ts
@@ -1,65 +1,146 @@
 export const home = {
+  link: '/',
   name: 'Home',
   title: 'MossAway | Roof Moss Treatment, Window & Gutter Cleaning in Victoria, BC',
-  link: '/',
   description: "MossAway offers award-winning roof moss treatment, window cleaning, and gutter cleaning services in Victoria, BC. Contact us for a free quote today!",
-  keywords: 'mossaway, roof moss treatment, window cleaning, gutter cleaning, moss removal, victoria bc'
+  keywords: 'mossaway, roof moss treatment, window cleaning, gutter cleaning, moss removal, victoria bc',
+  h1: 'MossAway Roof Moss Removal & Treatment',
 };
 
 export const mossRemoval = {
+  link: '/roof-moss-removal-victoria-bc',
   name: 'Roof Moss Removal',
-  title: 'Moss Removal Services | MossAway Victoria, BC',
-  link: '/roof-moss-removal',
-  description: 'Get professional moss removal services from MossAway in Victoria, BC. Safe, effective, and eco-friendly methods to remove moss from your roof.',
-  keywords: 'moss removal, moss removal services, roof moss removal, moss removal victoria bc',
+  title: 'Roof Moss Removal Services in Victoria, BC | MossAway',
+  description: 'Looking for expert roof moss removal services in Victoria, BC? MossAway uses a delicate hand brushing technique and pole brush work to safely and effectively remove moss from your roof. Our eco-friendly treatment solution, the West Coast Wash, ensures your roof stays moss-free for years to come. Request a free quote today!',
+  keywords: 'roof moss removal Victoria BC, delicate hand brushing, pole brush work, West Coast Wash, eco-friendly treatment solution',
+  h1: 'Roof Moss Removal Services in Victoria, BC | MossAway',
+  sections: [
+    {
+      title: 'Delicate Hand Brushing Technique',
+      content: "MossAway's roof moss removal process uses a delicate hand brushing technique in conjunction with our pole brush work.This allows us to safely and effectively remove moss from your roof without causing any damage.Once the moss is loose, we use a leaf blower to get all small moss particles, spores, etc.off the roof."
+    },
+    {
+      title: 'Gutter Cleaning and Cleanup',
+      content: 'As part of our roof moss removal and treatment service, we clean the gutters to ensure they are free from moss and debris. This is included in the price of our service. We also provide a detailed cleanup of the surrounding areas once the service is complete. We aim to remove as much moss as possible and leave the area clean and tidy.'
+    },
+    {
+      title: 'Eco-Friendly Treatment Solution',
+      content: 'Our roof moss treatment uses our roof-safe, effective, eco-friendly treatment solution, the West Coast Wash. This solution is made up of 2 chemicals that we use depending on the situation. Our hydrogen-peroxide based West Coast Wash is typically used, but occasionally we use a traditional soft wash solution for tough algae staining. Our evaluator will recommend the right chemical for the job and build your quote accordingly.'
+    },
+    {
+      title: 'Safety First',
+      content: 'Roof work can be dangerous, which is why we take safety very seriously. MossAway uses rope and harness as required by WorkSafeBC, as well as special feet on our ladders to provide extra stability and safety. We also use ladder standoffs which keep ladders from damaging gutter exteriors while also adding even more stability and safety to our ladder setups.'
+    },
+    {
+      title: 'Affordable Quality Service',
+      content: 'MossAway strives to be the most affordable quality service provider in Victoria, BC. We have been in the business for several years, and have earned a BBB A+ rating and the 2019 BBB Torch Award winners in specialty cleaning. We offer free quotes, and have excellent online reviews and ratings. We handle projects of all sizes, both residential and commercial.'
+    }
+  ]
 };
 
 export const roofMossTreatment = {
+  link: '/roof-moss-treatment-victoria-bc',
   name: 'Roof Moss Treatment',
-  title: 'Roof Moss Treatment | MossAway Victoria, BC',
-  link: '/roof-moss-treatment',
-  description: "Prevent roof moss from growing on your roof with MossAway's roof moss treatment. Safe, effective, and affordable. Contact us for a free quote today!",
-  keywords: 'roof moss treatment, moss treatment for roof, roof moss treatment victoria bc',
+  title: 'Roof Moss Treatment Services in Victoria, BC | MossAway',
+  description: 'Looking for expert roof moss treatment services in Victoria, BC? MossAway uses our roof-safe, effective, and eco-friendly treatment solution, the West Coast Wash, to keep your roof moss-free for years to come. Contact us today for a free quote!',
+  keywords: 'roof moss treatment Victoria BC, West Coast Wash, eco-friendly treatment solution',
+  h1: 'Roof Moss Treatment Services in Victoria, BC | MossAway',
+  sections: [
+    {
+      title: 'Our Treatment Process',
+      content: "MossAway's roof moss treatment process uses our roof-safe, effective, and eco-friendly treatment solution, the West Coast Wash. This solution is made up of 2 chemicals that we use depending on the situation. Our hydrogen-peroxide based West Coast Wash is typically used, but occasionally we use a traditional soft wash solution for tough algae staining. Our evaluator will recommend the right chemical for the job and build your quote accordingly. We apply the treatment solution evenly across the roof, ensuring complete coverage."
+    },
+    {
+      title: 'Eco-Friendly and Safe for Your Roof',
+      content: 'The West Coast Wash is a roof-safe and effective treatment solution that is eco-friendly and biodegradable. It won\'t harm your roof or the environment, and will keep your roof moss-free for years to come. Our team is trained and certified to handle the chemicals safely and responsibly, so you can trust us to do the job right.'
+    },
+    {
+      title: 'Affordable Quality Service',
+      content: 'MossAway strives to be the most affordable quality service provider in Victoria, BC. We have been in the business for several years, and have earned a BBB A+ rating and the 2019 BBB Torch Award winners in specialty cleaning. We offer free quotes, and have excellent online reviews and ratings. We handle projects of all sizes, both residential and commercial.'
+    }
+  ]
 };
 
 export const windowCleaning = {
+  link: '/window-cleaning-victoria-bc',
   name: 'Window Cleaning',
-  title: 'Window Cleaning Services | MossAway Victoria, BC',
-  link: '/window-cleaning',
-  description: 'Get professional window cleaning services from MossAway in Victoria, BC. Interior and exterior window cleaning available. Contact us for a free quote today!',
-  keywords: 'window cleaning, window cleaning services, professional window cleaning, window cleaning victoria bc',
+  title: 'Professional Window Cleaning Services in Victoria, BC | MossAway',
+  description: 'MossAway provides professional window cleaning services in Victoria, BC. Our hybrid approach of traditional window cleaning and waterfed pole ensures a clean finish, while our optional screen cleaning adds extra value. Contact us today for a free quote!',
+  keywords: 'window cleaning Victoria BC, waterfed pole, screen cleaning, commercial recurring services',
+  h1: 'Professional Window Cleaning Services in Victoria, BC',
+  sections: [
+    {
+      title: 'Our Window Cleaning Process',
+      content: 'MossAway uses a hybrid approach of traditional window cleaning and waterfed pole to provide a high-quality clean. Our waterfed pole system uses super pure water to wash away dirt, grime, and debris, leaving behind a clean, spot-free finish. We also offer optional screen cleaning, and if the customer removes the screens ahead of our service, we add in the screen cleaning for free. For properties with security screens, we may require inside access if the team is performing screen removal and cleaning services.'
+    },
+    {
+      title: 'Commercial Recurring Services',
+      content: 'MossAway offers commercial recurring services at a discounted rate, depending on the term of the contract. We always offer our first cleaning with no commitment so that our recurring service clients have the opportunity to evaluate our work before committing to the term. Residential properties are the only exception, as we only offer annual and semi-annual services, both of which are discounted to the same rate. For information on recurring service discounts, customers should call or email us.'
+    },
+    {
+      title: 'Interior and Exterior Services',
+      content: 'MossAway offers interior and exterior window cleaning services for properties up to 4 floors. For larger projects, we can refer our clients to one of our commercial window cleaning service partners in Victoria. We try to limit the amount of ladder window cleaning to as close to zero as possible, reducing the risk of accidental damage to the home and worker injury, providing our clients with extra safety and peace of mind.'
+    }
+  ]
 };
 
 export const gutterCleaning = {
+  link: '/gutter-cleaning-victoria-bc',
   name: 'Gutter Cleaning',
-  title: 'Gutter Cleaning Services | MossAway Victoria, BC',
-  link: '/gutter-cleaning',
-  description: 'Protect your home from water damage with professional gutter cleaning services from MossAway in Victoria, BC. Contact us for a free quote today!',
-  keywords: 'gutter cleaning, gutter cleaning services, expert gutter cleaning, gutter cleaning victoria bc',
+  title: 'Gutter Cleaning in Victoria, BC | MossAway',
+  description: 'Looking for expert gutter cleaning services in Victoria, BC? MossAway uses specialized high-powered gutter vacuums and rope & harness cleaning to provide a fast, safe, and thorough clean. Request a free quote today!',
+  keywords: 'gutter cleaning Victoria BC, specialized gutter vacuums, rope and harness cleaning, Greater Victoria Region',
+  h1: 'Gutter Cleaning Services in Victoria, BC | MossAway',
+  sections: [
+    {
+      title: 'Specialized Gutter Vacuuming Services',
+      content: 'MossAway uses specialized high-powered gutter vacuums to remove debris and buildup from gutters quickly and effectively. This method reduces the likelihood of damage to the gutters, surrounding roof, and property, and allows us to reach places that other companies can’t. We also offer traditional hand-cleaning for hard-to-reach areas.'
+    },
+    {
+      title: 'Rope & Harness Cleaning',
+      content: 'Our staff are trained and certified to use rope and harness equipment as required by WorkSafeBC. This ensures that our team can safely access and clean gutters on high buildings, steep roofs, and other challenging locations. This approach reduces the risk of injury and property damage, and helps us deliver high-quality results on every job.'
+    },
+    {
+      title: 'Gutter Cleaning for the Greater Victoria Region',
+      content: 'MossAway serves the Greater Victoria Region, including various municipalities and neighborhoods. We offer free quotes for all projects, big and small, and strive to provide the most affordable quality service in the region. Contact us today to schedule your gutter cleaning appointment!'
+    }
+  ]
 };
 
 export const faq = {
-  name: 'FAQ',
-  title: 'Frequently Asked Questions | MossAway Victoria, BC',
   link: '/faq',
-  description: 'Get answers to common questions about our services and read our latest blog posts on the MossAway FAQ page. Contact us for more information.',
-  keywords: 'mossaway faq, mossaway blog, mossaway news, mossaway updates, mossaway victoria bc',
+  name: 'FAQ & Promotions',
+  title: 'Frequently Asked Questions, Promotions, & Blog | MossAway',
+  description: 'Find answers to frequently asked questions about exterior cleaning services and stay up-to-date with the latest events, announcements, and promotions from MossAway. Subscribe to our newsletter to receive exclusive discounts and offers!',
+  keywords: 'exterior cleaning FAQs, blog, events, promotions, newsletter',
+  h1: 'Frequently Asked Questions and Blog',
 };
 
 export const contact = {
-  name: 'Contact',
-  title: 'Contact MossAway | MossAway Victoria, BC',
   link: '/contact',
-  description: 'Get in touch with MossAway in Victoria, BC for a free quote on our moss removal, roof moss treatment, window cleaning, and gutter cleaning services.',
-  keywords: 'contact mossaway, mossaway contact, mossaway victoria bc',
+  name: 'Contact Us',
+  title: 'Contact Us | MossAway',
+  description: 'Contact MossAway for all your exterior cleaning needs. Fill out our contact form or give us a call to schedule your free quote today!',
+  keywords: 'contact us, exterior cleaning, free quote',
+  h1: 'Contact MossAway for Exterior Cleaning Services',
 };
 
 export const reviews = {
-  name: 'Reviews',
-  title: 'MossAway Reviews | Victoria, BC',
   link: '/reviews',
-  description: 'Read MossAway reviews and ratings from satisfied customers in Victoria, BC. Contact us for a free quote on our moss removal, roof moss treatment, window cleaning, and gutter cleaning services.',
-  keywords: 'mossaway reviews, mossaway ratings, mossaway victoria bc',
+  name: 'Testimonials',
+  title: 'Reviews & Testimonials | MossAway',
+  description: 'Check out what our satisfied customers have to say about our exterior cleaning services. Read our reviews and testimonials, and see why MossAway is the best choice for roof moss removal, roof moss treatment, gutter cleaning, and window cleaning in the Greater Victoria Region!',
+  keywords: 'reviews, testimonials, exterior cleaning, customer feedback',
+  h1: 'Customer Reviews and Testimonials',
+};
+
+export const requestQuote = {
+  link: '/request-quote',
+  name: 'Request a Quote',
+  title: 'Request a Quote | MossAway',
+  description: 'Request a free quote for exterior cleaning services from MossAway. Fill out our contact form or give us a call to schedule your free quote today!',
+  keywords: 'request a quote, exterior cleaning, free quote',
+  h1: 'Request a Free Quote for Exterior Cleaning Services',
 };
 
 export const pages =
