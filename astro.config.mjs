4c7d078b552262cbdf69f3101278896fdd9941bb
Roga <ryanroga@gmail.com>
2023-05-09 17:55:46 -0700

    Properly added sitemap
    Using `npx astro add sitemap`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import svelte from '@astrojs/svelte'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://victoria.mossaway.ca',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()],
-})
+  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+});
